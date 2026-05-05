ecd9f2f33c32f2be16e191799acea7f8390efe88
Slava Kihta <vaceslavkihta@gmail.com>
2026-05-05 06:54:24 +0500

    [ver 0.3.1] fixed Big rotated

--- DataStructures_main/DataStructures_main.cpp
+++ DataStructures_main/DataStructures_main.cpp
@@ -3,17 +3,11 @@
 
 using namespace DataStructures;
 
-template<typename KeyT>
-using node = DataStructures::Node<KeyT>;
-
-using col = DataStructures::Colours;
-
 int main()
 {
-    node<int>* a = new Node<int>{ 2, nullptr, nullptr, nullptr, col::red },
-        *b = new Node<int>{ 1, nullptr, nullptr, nullptr, col::black };
-
-    DataStructures::reverseColour(a, b);
-    cout << a->colour << '\t' << b->colour;
+    rbt<int> myTree;
+    for (int i = 1; i <= 8; ++i) {
+        myTree.insert(i);
+    }
+    myTree.print();
 }
-
--- DataStructures_main/RBT.h
+++ DataStructures_main/RBT.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <iostream>
-#include <memory>
+#include <queue>
 #include <initializer_list>
-#include <exception>
+#include <Windows.h>
+#include <string>
 
 using std::cout, std::endl, std::cerr;
-using std::unique_ptr, std::make_unique;
+using std::queue;
 using std::initializer_list;
+using std::string;
 
 namespace DataStructures {
 	enum Colours { black, red };
@@ -24,7 +26,15 @@ namespace DataStructures {
 		std::size_t sz = 0;
 
 		struct iterator;
-		iterator find_placement(const KeyT&);
+		iterator find_placement(const KeyT& key) {
+			Node<KeyT>* currNode = root;
+			while (currNode != nullptr) {
+				if (key > currNode->key && currNode->right != nullptr) { currNode = currNode->right; }
+				else if (key < currNode->key && currNode->left != nullptr) { currNode = currNode->left; }
+				else break;
+			}
+			return currNode == nullptr ? iterator(root) : iterator(currNode);
+		}
 
 		void fixInsert(Node<KeyT>* node) {
 			bool isLeft = false;
@@ -40,8 +50,13 @@ namespace DataStructures {
 	
 					if (isNode(g->parent) && isRed(g->parent)) { fixInsert(g->parent); }// какую ноду передавать?
 				}
+				else if (!isNode(u)) {
+					if (isLeft) { RRB(node); }
+					else { LRB(node); }
+					reverseColour(g, node);
+				}
 				else if (isNode(u) && isBlack(u)) {
-					Node<KeyT>* ch = (node->left && node->left->colour == red) ? node->left : node->right;
+					Node<KeyT>* ch = (isNode(node->left) && isRed(node->left)) ? node->left : node->right;
 
 					if (isLeft) {
 						if (iAmLeft(ch, node)) { RRB(node); reverseColour(node, g); }
@@ -54,30 +69,144 @@ namespace DataStructures {
 				}
 			}
 		}
+		void fixErase(Node<KeyT>* node) {
+			Node<KeyT>* p = node->parent;
+
+			if (node->colour == red && node->right == nullptr && node->left == nullptr) { delete node; return; } // V1
+			else if (node->colour == black) { // V2
+				bool isLeft = false;  // true: deletion node is p->right
+				Node<KeyT>* ch;        // [Дублирующая проверка]
+
+				if (node == p->right) { ch = p->right; isLeft = 1; }
+				else { ch = p->left; }
+
+				if (isRed(p)) { // V2.1
+					if (isNode(ch)) {
+						if (!isLeft && isLeftCh(ch)) { RR(ch); LRB(p->right); } // Дублирующия проверка
+						else if (isLeft && isRightCh(ch)) { LR(ch); RRB(p->left); } // возможно условие избыточно
+					}
+					else { reverseColour(p, ch); }
+					delete node;
+				}
+				else { // V2.2 
+					if (isNode(ch) && isRed(ch)) {  // возможно условие избыточно
+						Node<KeyT>* d = isLeft ? ch->right->left : ch->left->right;
+
+						if (isNode(d) && isRed(d)) { // isRed возможно избыточно
+							if (isLeft) { LR(ch); RRB(p); }
+							else { RR(ch); LRB(p); }
+						}
+						else {
+							if (isLeft) { RRB(ch); }
+							else { LRB(ch); }
+							reverseColour(d);
+						}
+						delete node;
+					}
+					else if (isNode(ch) && isBlack(ch)) {
+						Node<KeyT>* c = isLeft ? ch->right : ch->left;
+						if (isNode(c)) {
+							if (isLeft) { RR(ch); LRB(c); }
+							else { LR(ch); RRB(c); }
+							reverseColour(c);
+						}
+						else { reverseColour(ch); }
+						delete node;
+					}
+				}
+			}
+		}
+
+		void LR(Node<KeyT>* node) {
+			Node<KeyT>* x = node->right, * ch = node->left;
+			x->parent = node->parent;
+			node->parent->left = x;
+			if (isNode(ch)) {
+				node->parent = ch;
+				ch->left = node;
+			}
+			else {
+				node->parent = x;
+				x->left = node;
+			}
+		}
+		void RR(Node<KeyT>* node) {
+			Node<KeyT>* x = node->left, * ch = node->right;
+			x->parent = node->parent;
+			node->parent->right = x;
+			if (isNode(ch)) {
+				node->parent = ch;
+				ch->right = node;
+			}
+			else {
+				node->parent = x;
+				x->right = node;
+			}
+		}
+		void RRB(Node<KeyT>* node) {
+			Node<KeyT>* child = node->right,
+				* ggf = node->parent->parent != 0 ? node->parent->parent : nullptr;
+			node->right = node->parent;
+			if (ggf != nullptr) {
+				node->parent = ggf;
+				node == ggf->left ? ggf->left = node : ggf->right = node;
+			}
+			else { root = node; node->parent = nullptr;	}
+			node->right->parent = node;
+		
+			if (child != nullptr) {
+				node->right->left = child;
+				child->parent = node->right;
+			}
+			else{ node->right->left = nullptr; }
+			
+		}
+		void LRB(Node<KeyT>* node) {
+			Node<KeyT>* child = node->left,
+				* ggf = node->parent->parent != 0 ? node->parent->parent : nullptr;
+			node->left = node->parent;
+			if (ggf != nullptr) {
+				node->parent = ggf;
+				node == ggf->left ? ggf->left = node : ggf->right = node;
+			}
+			else { root = node; node->parent = nullptr; }
+			node->left->parent = node;
+			
+			if (child != nullptr) {
+				node->left->right = child;
+				child->parent = node->left;
+			}
+			else{ node->left->right = nullptr; }
+		}
 
 	public:
 		struct iterator;
 
-		rbt(KeyT);
-		rbt(initializer_list<KeyT>);
+		rbt() :root(nullptr), sz(0) { ; }
+		rbt(KeyT key) { insert(key); }
+		rbt(initializer_list<KeyT> list) {
+			for (auto&& key : list) {
+				insert(key);
+			}
+		}
 		rbt(const rbt&);
-		rbt(rbt&&);
+		rbt(rbt&& rhs) noexcept : root(rhs.root), sz(rhs.sz) {
+			rhs.root = nullptr;
+			rhs.sz = 0;
+		}
 
 		rbt& operator=(const rbt&);
-		rbt& operator=(rbt&&);
+		rbt& operator=(rbt&& rhs) noexcept {
+			root = rhs.root; sz = rhs.sz;
+			rhs.root = nullptr; rhs.sz = 0;
+		}
 
-		~rbt();
-
-		size_t size() const;
-		bool empty() const;
-
-		void insert(const KeyT&);
-		void erase(const KeyT&);
-
-		iterator find(const KeyT&);
-
-		iterator begin();
-		iterator end();
+		/*~rbt() {
+			for (auto it = begin(), itNext = ++it, itEnd = end(); it != itEnd; ++it, ++itNext) {
+				delete it.getNode();
+				it = itNext;
+			}
+		}*/
 
 		struct iterator {
 			using difference_type = std::ptrdiff_t;
@@ -92,8 +221,31 @@ namespace DataStructures {
 			iterator(Node<KeyT>* src) : node(src) { ; }
 			iterator(const iterator& rhs) : node(rhs.node) { ; }
 
-			void increment();
-			void decrement();
+			void increment() {
+				if (node->right != nullptr) {
+					node = node->right;
+					while (node->left != nullptr) { node = node->left; }
+				}
+				else {
+					Node<KeyT>* tmpParent = node->parent;
+					while (node == tmpParent->right) {
+						node = tmpParent;
+						tmpParent = tmpParent->parent;
+					}
+					if (node->right != tmpParent) { node = tmpParent; }
+				}
+			}
+			void decrement() {
+				Node<KeyT>* tmpParent = node->parent;
+				if (node->left == nullptr) {
+					while (node == tmpParent->left) {
+						node = tmpParent;
+						tmpParent = node->parent;
+					}
+					node = tmpParent->right;
+				}
+				else { node = node->left; }
+			}
 
 			iterator& operator++() { increment(); return *this; }
 			iterator& operator++(int) {
@@ -107,117 +259,102 @@ namespace DataStructures {
 				decrement();
 				return tmp;
 			}
-			reference operator*() { return node->key; }
+			reference operator*() { return node->key; } // Сомнительно
+
+			bool operator==(iterator rhs) { return node == rhs.node; }
+			bool operator!=(iterator rhs) { return !(node == rhs.node); }
+
+			Node<KeyT>* getNode() const { return node; }
 		};
+
+		size_t size() const { return sz; }
+		bool empty() const { return sz == 0 ? 1 : 0; }
+
+		void insert(const KeyT& key) {
+			if (sz > 0) {
+				Node<KeyT>* place = find_placement(key).getNode();
+				if (key > place->key) { place->right = new Node<KeyT>{ key, place, nullptr, nullptr, red }; }
+				else if (key < place->key) { place->left = new Node<KeyT>{ key, place, nullptr, nullptr, red }; }
+				
+				if(isRed(place)){ fixInsert(place); }
+			}
+			else {
+				root = new Node<KeyT>{ key, nullptr, nullptr, nullptr, black };
+			}
+			++sz;
+		}
+		void erase(const KeyT& key) {
+			auto deletionNode = find(key).getNode();
+			fixErase(deletionNode);
+			--sz;
+		}
+		
+		void print() {
+			queue<Node<KeyT>*> q;
+			q.push(root);
+			while (!q.empty()) {
+				int levelSize = q.size();
+
+				for (int i = 0; i < levelSize; ++i) {
+					auto node = q.front();
+					q.pop();
+
+					if (isRed(node)) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4); }
+					else { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7); }
+
+					Node<KeyT>* p = node->parent ? node->parent : nullptr,
+						* leftCh = node->left ? node->left : nullptr,
+						* rightCh = node->right ? node->right : nullptr;
+					bool isLeft = false;
+
+					if (isNode(p) && iAmLeft(node, p)) { isLeft = true; }
+					string dir = isLeft ? " (L)" : " (R)";
+					if (node == root) { dir = ""; }					
+					
+					cout << node->key << " (P: " << getKey(p) << dir << "; L: " << getKey(leftCh)
+						<< "; R: " << getKey(rightCh) << ")" << "\t";
+
+					if (isNode(node->left)) { q.push(node->left); }
+					if (isNode(node->right)) { q.push(node->right); }
+				}
+				cout << endl;
+			}
+		}
+
+		void printList() {
+			queue<Node<KeyT>*> q;
+			q.push(root);
+			while (!q.empty()) {
+				int levelSize = q.size();
+
+				auto node = q.front();
+				q.pop();
+
+				cout << node->key << " ";
+
+				if (isNode(node->left)) { q.push(node->left); }
+				if (isNode(node->right)) { q.push(node->right); }
+			}
+		}
+		
+		iterator find(const KeyT& key) { // замерить с проверкой left/right перед переходом и без нее
+			Node<KeyT>* currNode = root;
+			while (currNode->key != key) {
+				if (currNode->key > key) { currNode = currNode->left; }
+				else if (currNode->key < key) { currNode = currNode->right; }
+			}
+			return currNode == nullptr ? iterator(root) : iterator(currNode);
+		}
+
+		iterator begin() {
+			auto currNode = root;
+			while (isLeftCh(currNode)) {
+				currNode = currNode->left;
+			}
+			return iterator(currNode);
+		}
+		iterator end() { return iterator(root); }
 	};
-
-	template <typename KeyT>
-	void LR(Node<KeyT>* node) {
-		Node<KeyT>* x = node->right, * ch = node->left;
-		x->parent = node->parent;
-		node->parent->left = x;
-		if (isNode(ch)){
-			node->parent = ch;
-			ch->left = node;
-		}
-		else {
-			node->parent = x;
-			x->left = node;
-		}
-	}
-
-	template <typename KeyT>
-	void RR(Node<KeyT>* node) {
-		Node<KeyT>* x = node->left, *ch = node->right;
-		x->parent = node->parent;
-		node->parent->right = x;
-		if (isNode(ch)) {
-			node->parent = ch;
-			ch->right = node;
-		}
-		else {
-			node->parent = x;
-			x->right = node;
-		}		
-	}
-
-	template <typename KeyT>
-	void RRB(Node<KeyT>* node) {
-		Node<KeyT>* child = node->right,
-			* ggf = node->parent->parent != 0 ? node->parent->parent : nullptr;
-		node->right = node->parent;
-		if (ggf != nullptr) {
-			node->parent = ggf;
-			node == ggf->left ? ggf->left = node : ggf->right = node;
-		}
-		node->right->parent = node;
-		node->right->left = child;
-		child->parent = node->right;
-	}
-
-	template <typename KeyT>
-	void LRB(Node<KeyT>* node) {
-		Node<KeyT>* child = node->left,
-			* ggf = node->parent->parent != 0 ? node->parent->parent : nullptr;
-		node->left = node->parent;
-		if (ggf != nullptr) {
-			node->parent = ggf;
-			node == ggf->left ? ggf->left = node : ggf->right = node;
-		}
-		node->left->parent = node;
-		node->right->left = child;
-		child->parent = node->right;
-	}	
-
-	template <typename KeyT>
-	void erase(Node<KeyT>* node) {
-		Node<KeyT>* p = node->parent;
-
-		if (node->colour == red && node->right == nullptr && node->left == nullptr) { delete node; return; } // V1
-		else if (node->colour == black) { // V2
-			bool isLeft = false;  // true: deletion node is p->right
-			Node<KeyT>* ch;        // [Дублирующая проверка]
-
-			if (node == p->right) { ch = p->right; isLeft = 1; }
-			else { ch = p->left; }
-
-			if (isRed(p)) { // V2.1
-				if (isNode(ch)) {
-					if (!isLeft && isLeftCh(ch)) { RR(ch); LRB(p->right); } // Дублирующия проверка
-					else if (isLeft && isRightCh(ch)) { LR(ch); RRB(p->left); } // возможно условие избыточно
-				}
-				else { reverseColour(p, ch); }
-				delete node;
-			}
-			else { // V2.2 
-				if (isNode(ch) && isRed(ch)) {  // возможно условие избыточно
-					Node<KeyT>* d = isLeft ? ch->right->left : ch->left->right;
-
-					if (isNode(d) && isRed(d)) { // isRed возможно избыточно
-						if (isLeft) { LR(ch); RRB(p); }
-						else { RR(ch); LRB(p); }
-					}
-					else {
-						if (isLeft) { RRB(ch); }
-						else { LRB(ch); }
-						reverseColour(d);
-					}
-				}
-				else if (isNode(ch) && isBlack(ch)) {
-					Node<KeyT>* c = isLeft ? ch->right : ch->left;
-					if (isNode(c)) {
-						if (isLeft) { RR(ch); LRB(c); }
-						else { LR(ch); RRB(c); }
-						reverseColour(c);
-					}
-					else { reverseColour(ch); }
-				}
-			}
-		}
-	}
-
-	template<typename KeyT>
-	void balancing(Node<KeyT>*);
 
 	template <typename KeyT>
 	void revCol(Node<KeyT>* node) {
@@ -251,4 +388,9 @@ namespace DataStructures {
 	template <typename KeyT>
 	bool isBlack(Node<KeyT>* node) { return node->colour == black ? true : false; }
 
+	template <typename KeyT>
+	string getKey(Node<KeyT>* node){
+		if (isNode(node)) { return std::to_string(node->key); }
+		else { return "nil"; }
+	}
 }
